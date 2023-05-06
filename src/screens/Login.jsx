4c0affda4685691fd4a050c8a771505fd265f265
Bajmo <anas.mouradd@gmail.com>
2023-05-06 02:07:46 +0100

    Added basic input validation

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,35 +1,57 @@
-import React, { useState } from 'react';
-import '../css/login.css';
-import Logo from '../assets/logo.png';
-import axios from 'axios';
+import React, { useState } from "react";
+import "../css/login.css";
+import Logo from "../assets/logo.png";
+import axios from "axios";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  function validateInputs() {
+    let valid = true;
+    if (username.trim() === "") {
+      setUsernameError("Username is required");
+      valid = false;
+    } else {
+      setUsernameError("");
+    }
+    if (password.trim() === "") {
+      setPasswordError("Password is required");
+      valid = false;
+    } else {
+      setPasswordError("");
+    }
+    return valid;
+  }
 
   const handleSubmit = async (event) => {
     event.preventDefault(); // prevent default form submission
+    if (!validateInputs()) {
+      return;
+    }
     try {
       // Authenticate the user and retrieve the token
       const token = await authenticateUser(username, password);
 
       // Set the token in the local storage
-      localStorage.setItem('token', token);
+      localStorage.setItem("token", token);
 
       // Redirect the user to the dashboard page
-      window.location.href = '/';
+      window.location.href = "/";
     } catch (error) {
       console.error(error);
-      alert('Invalid username or password');
+      alert("Invalid username or password");
     }
   };
 
   async function authenticateUser(username, password) {
     try {
       // Send a POST request to the ClientUserLogin endpoint to authenticate the user
-      const response = await axios.post('http://localhost:8000/signin', {
+      const response = await axios.post("http://localhost:8000/signin", {
         username: username,
-        password: password
+        password: password,
       });
 
       // Extract the token from the response and return it
@@ -42,7 +64,9 @@ const Login = () => {
 
   return (
     <div className="loginpage flex items-center justify-center">
-      <form onSubmit={handleSubmit}> {/* add onSubmit handler */}
+      <form onSubmit={handleSubmit}>
+        {" "}
+        {/* add onSubmit handler */}
         <div className="loginbox w-80 bg-white flex flex-col items-center rounded-3xl p-8">
           <img className="w-24" src={Logo} />
           <p className="p-4 font-semibold text-xl">Authenticate</p>
@@ -52,12 +76,15 @@ const Login = () => {
             type="text"
             placeholder="Username"
           />
+          {usernameError && <p className="mt-1 text-xs text-red-500">{usernameError}</p>}
           <input
             onChange={(e) => setPassword(e.target.value)}
             className="border border-black w-56 rounded-3xl placeholder:text-xs px-3 placeholder:text-red-600 py-2 mt-2"
             type="password"
             placeholder="Password"
           />
+          {passwordError && <p className="mt-1 text-xs text-red-500">{passwordError}</p>}
+
           <button
             type="submit" // change type to submit
             className="text-white bg-black rounded-3xl w-56 py-3 text-sm mt-4"
@@ -65,7 +92,13 @@ const Login = () => {
             Log in
           </button>
           <p className="mt-4 text-xs">Don't have an account yet?</p>
-          <span className='text-xs'>Click <a href="/Register" className=' text-red-600'>here</a> to make one.</span>
+          <span className="text-xs">
+            Click{" "}
+            <a href="/Register" className=" text-red-600">
+              here
+            </a>{" "}
+            to make one.
+          </span>
         </div>
       </form>
     </div>
--- src/screens/Register.jsx
+++ src/screens/Register.jsx
@@ -9,9 +9,56 @@ const Register = () => {
   const [username,setUsername]=useState("")
   const [email,setEmail]=useState("")
   const [password,setPassword]=useState("")
+  const [firstnameError, setFirstnameError] = useState("");
+  const [lastnameError, setLastnameError] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const emailRegex = /\S+@\S+\.\S+/;
+
+  function validateInputs() {
+    let valid = true;
+    if (firstname.trim() === "") {
+      setFirstnameError("First name is required");
+      valid = false;
+    } else {
+      setFirstnameError("");
+    }
+    if (lastname.trim() === "") {
+      setLastnameError("Last name is required");
+      valid = false;
+    } else {
+      setLastnameError("");
+    }
+    if (username.trim() === "") {
+      setUsernameError("Username is required");
+      valid = false;
+    } else {
+      setUsernameError("");
+    }
+    if (email.trim() === "") {
+      setEmailError("Email is required");
+      valid = false;
+    } else if (!emailRegex.test(email)) {
+      setEmailError("Invalid email");
+      valid = false;
+    } else {
+      setEmailError("");
+    }
+    if (password.trim() === "") {
+      setPasswordError("Password is required");
+      valid = false;
+    } else {
+      setPasswordError("");
+    }
+    return valid;
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault(); // prevent default form submission
+    if (!validateInputs()) {
+      return;
+    }
     const body = {
       "first_name": firstname,
       "last_name": lastname,
@@ -42,30 +89,35 @@ const Register = () => {
             placeholder="First name"
             onChange={(e)=>setFirstname(e.target.value)}
           />
+          {firstnameError && <p className="mt-1 text-xs text-red-500">{firstnameError}</p>}
           <input
             className="border border-black w-56 rounded-3xl placeholder:text-xs px-3 placeholder:text-red-600 py-2 mt-2"
             type="text"
             placeholder="Last name"
             onChange={(e)=>setLastname(e.target.value)}
           />
+          {lastnameError && <p className="mt-1 text-xs text-red-500">{lastnameError}</p>}
           <input
             className="border border-black w-56 rounded-3xl placeholder:text-xs px-3 placeholder:text-red-600 py-2 mt-2"
             type="text"
             placeholder="Username"
             onChange={(e)=>setUsername(e.target.value)}
           />
+          {usernameError && <p className="mt-1 text-xs text-red-500">{usernameError}</p>}
           <input
             className="border border-black w-56 rounded-3xl placeholder:text-xs px-3 placeholder:text-red-600 py-2 mt-2"
             type="text"
             placeholder="Email"
             onChange={(e)=>setEmail(e.target.value)}
           />
+          {emailError && <p className="mt-1 text-xs text-red-500">{emailError}</p>}
           <input
             className="border border-black w-56 rounded-3xl placeholder:text-xs px-3 placeholder:text-red-600 py-2 mt-2"
-            type="text"
+            type="password"
             placeholder="Password"
             onChange={(e)=>setPassword(e.target.value)}
           />
+          {passwordError && <p className="mt-1 text-xs text-red-500">{passwordError}</p>}
           <button type="submit" className="text-white bg-black rounded-3xl w-56 py-3 text-sm mt-4"> {/* change type to submit */}
             Sign up
           </button>
