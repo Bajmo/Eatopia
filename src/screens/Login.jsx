ef191324f7914e9213d5d37b6640d621d0e06096
Bajmo <anas.mouradd@gmail.com>
2023-05-06 01:16:40 +0100

    Modified the Login and Register screens

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,29 +1,72 @@
-import React ,{useState}from 'react'
-import '../css/login.css'
-import Logo from '../assets/logo.png'
-import axios from 'axios'
-const Login = () => {
-  const [username,setUsername]=useState("")
-  const [password,setPassword]=useState("")
+import React, { useState } from 'react';
+import '../css/login.css';
+import Logo from '../assets/logo.png';
+import axios from 'axios';
 
-  const handlePress =()=>{
-    const body={
-        "username":username,
-        "password":password
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handlePress = async () => {
+    try {
+      // Authenticate the user and retrieve the token
+      const token = await authenticateUser(username, password);
+
+      // Set the token in the local storage
+      localStorage.setItem('token', token);
+
+      // Redirect the user to the dashboard page
+      window.location.href = '/home';
+    } catch (error) {
+      console.error(error);
+      alert('Invalid username or password');
     }
-    axios.post("/endpoint",body)
-}
+  };
+
+  async function authenticateUser(username, password) {
+    try {
+      // Send a POST request to the ClientUserLogin endpoint to authenticate the user
+      const response = await axios.post('http://localhost:8000/signin', {
+        username: username,
+        password: password
+      });
+
+      // Extract the token from the response and return it
+      return response.data.token;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
   return (
-    <div className='loginpage flex items-center justify-center'>
-      <div className='loginbox w-80 h-96 bg-white flex flex-col items-center rounded-3xl p-8'>
-        <img className='w-24' src={Logo}/>
-        <p className='p-4 font-semibold text-xl'>Welcome back !</p>
-        <input onChange={(e)=>setUsername(e.target.value)} className='border border-black w-56 rounded-3xl placeholder:text-xs placeholder:px-3 placeholder:text-red-600 py-2' type='text' placeholder='Username'/>
-        <input onChange={(e)=>setPassword(e.target.value)} className='border border-black w-56 rounded-3xl placeholder:text-xs placeholder:px-3 placeholder:text-red-600 py-2 mt-2' type='text' placeholder='Password'/>
-        <button className='text-white bg-black rounded-3xl w-56 py-3 text-sm mt-4'>Log In</button>
+    <div className="loginpage flex items-center justify-center">
+      <div className="loginbox w-80 bg-white flex flex-col items-center rounded-3xl p-8">
+        <img className="w-24" src={Logo} />
+        <p className="p-4 font-semibold text-xl">Authenticate</p>
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          className="border border-black w-56 rounded-3xl placeholder:text-xs px-3 placeholder:text-red-600 py-2"
+          type="text"
+          placeholder="Username"
+        />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          className="border border-black w-56 rounded-3xl placeholder:text-xs px-3 placeholder:text-red-600 py-2 mt-2"
+          type="password"
+          placeholder="Password"
+        />
+        <button
+          onClick={handlePress}
+          className="text-white bg-black rounded-3xl w-56 py-3 text-sm mt-4"
+        >
+          Log In
+        </button>
+        <p className="mt-4 text-xs">Don't have an account yet?</p>
+        <span className='text-xs'>Click <a href="/Register" className=' text-red-600'>here</a> to make one.</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/screens/Register.jsx
+++ src/screens/Register.jsx
@@ -9,47 +9,56 @@ const Register = () => {
     const [email,setEmail]=useState("")
     const [password,setPassword]=useState("")
 
-    const handlePress =()=>{
-        const body={
-            "first_name":firstname,
-            "last_name":lastname,
-            "username":username,
-            "email":email,
-            "password":password
-        }
-        axios.post("http://127.0.0.1:8000/signup",body)
-    }
+    const handlePress = () => {
+      const body = {
+        "first_name": firstname,
+        "last_name": lastname,
+        "username": username,
+        "email": email,
+        "password": password
+      };
+      axios.post("http://127.0.0.1:8000/signup", body)
+        .then((response) => {
+          // Handle success
+        })
+        .catch((error) => {
+          // Handle error
+        });
+      console.log(body);
+    };
+    
   return (
     <div className="loginpage flex items-center justify-center">
-      <div className="loginbox w-80 h-96 bg-white flex flex-col items-center rounded-3xl p-8">
+      <div className="loginbox w-80 bg-white flex flex-col items-center rounded-3xl p-8">
         <img className="w-24" src={Logo} />
-        <p className="p-4 font-semibold text-xl">Register</p>
+        <p className="m-4 font-semibold text-xl">Sign up</p>
+        <hr/>
         <input
-          className="border border-black w-56 rounded-3xl placeholder:text-xs placeholder:px-3 placeholder:text-red-600 py-2"
+          className="border border-black w-56 rounded-3xl placeholder:text-xs px-3 placeholder:text-red-600 py-2"
           type="text"
           placeholder="First name"
           onChange={(e)=>setFirstname(e.target.value)}
         />
         <input
-          className="border border-black w-56 rounded-3xl placeholder:text-xs placeholder:px-3 placeholder:text-red-600 py-2"
+          className="border border-black w-56 rounded-3xl placeholder:text-xs px-3 placeholder:text-red-600 py-2 mt-2"
           type="text"
           placeholder="Last name"
           onChange={(e)=>setLastname(e.target.value)}
         />
         <input
-          className="border border-black w-56 rounded-3xl placeholder:text-xs placeholder:px-3 placeholder:text-red-600 py-2"
+          className="border border-black w-56 rounded-3xl placeholder:text-xs px-3 placeholder:text-red-600 py-2 mt-2"
           type="text"
           placeholder="Username"
           onChange={(e)=>setUsername(e.target.value)}
         />
         <input
-          className="border border-black w-56 rounded-3xl placeholder:text-xs placeholder:px-3 placeholder:text-red-600 py-2"
+          className="border border-black w-56 rounded-3xl placeholder:text-xs px-3 placeholder:text-red-600 py-2 mt-2"
           type="text"
           placeholder="Email"
           onChange={(e)=>setEmail(e.target.value)}
         />
         <input
-          className="border border-black w-56 rounded-3xl placeholder:text-xs placeholder:px-3 placeholder:text-red-600 py-2 mt-2"
+          className="border border-black w-56 rounded-3xl placeholder:text-xs px-3 placeholder:text-red-600 py-2 mt-2"
           type="text"
           placeholder="Password"
           onChange={(e)=>setPassword(e.target.value)}
@@ -57,6 +66,8 @@ const Register = () => {
         <button onClick={handlePress} className="text-white bg-black rounded-3xl w-56 py-3 text-sm mt-4">
           Register
         </button>
+        <p className="mt-4 text-xs">Already have an account?</p>
+        <span className='text-xs'>Click <a href="/Login" className=' text-red-600'>here</a> to sign in.</span>
       </div>
     </div>
   );
